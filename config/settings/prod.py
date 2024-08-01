3f84fe5249173c24996a456413c0fcf45d63f5ec
정하민 <hamin220369@gmail.com>
2024-08-01 17:18:44 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = []
 
 
 # Default primary key field type
@@ -141,3 +139,5 @@ pymysql.install_as_MySQLdb()
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = BASE_DIR / 'static/'
