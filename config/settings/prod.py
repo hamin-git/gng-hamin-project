e397b08fbe1f98ab1c62bfcd09dcc1378ca1d04f
정하민 <hamin220369@gmail.com>
2024-08-19 12:12:52 +0900

    settings 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -120,9 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Default primary key field type
